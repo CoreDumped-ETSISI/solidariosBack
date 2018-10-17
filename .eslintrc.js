9defa0cdfe258662ddc8c33dcdc379e6304b79b3
Xavikh <xavikh@gmail.com>
2018-10-17 23:56:54 +0200

    Added ESlint style

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "warn",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "warn",
+            {
+                allow: [
+                    "warn"
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        "json"
+    ]
+};

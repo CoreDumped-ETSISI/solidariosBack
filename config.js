5f8db6605b7b8dd5ad53f12b13448d5d644d0f4f
Xavikh <xavikh@gmail.com>
2018-10-20 20:46:09 +0200

    Added config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    EXP_DAYS: 30,
+
+    RESTORE_PASS_EXP: 1,      //hours
+    VERIFY_EMAIL_EXP: 24,     //hours
+
+    PASS_MIN_LENGTH: 8,
+    PASS_MAX_LENGTH: 30,
+
+    PREDEFINED_USER_IMAGE: ''
+};

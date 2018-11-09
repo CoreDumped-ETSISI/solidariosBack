947c4c803b24c3e302248591a983fa661b73a3a8
raulgarcia <insepal1998@gmail.com>
2018-11-09 06:14:44 +0100

    Fixed eTypes

--- models/eType.js
+++ models/eType.js
@@ -6,3 +6,5 @@ const Schema = mongoose.Schema;
 const eTypeSchema= new Schema({
     value: {type: String, text: true, required: true}
 })
+
+module.exports = mongoose.model('eTypes', eTypeSchema);
--- routes/eTypesRoutes.js
+++ routes/eTypesRoutes.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 const eTypeController = require('../controllers/eTypeController');
+const authorise = require('../middlewares/authorise');
+const admin = require('../middlewares/admin');
 
 router.post('/create', authorise, admin, eTypeController.createType);
 router.get('/list', authorise, admin, eTypeController.getEtypes);

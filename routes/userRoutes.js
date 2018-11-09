4721ce2685a98bbea7f2e1b41f69635ffba7e6bb
Luis de la Cal <luiscal4a@gmail.com>
2018-11-09 07:14:42 +0100

    Añadidos validators a los datos pasados por el body

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,25 @@ const authorise = require('../middlewares/authorise');
 const admin = require('../middlewares/admin');
 // const needer = require('../middlewares/needer');
 // const volunteer = require('../middlewares/volunteer');
+const { body } = require('express-validator/check');
 
 //public
-router.post('/register', userController.signUp); //ok
-router.post('/login', userController.login); //
+router.post('/register',  [
+    body('name').isLength({ min: 2, max: 40}),
+    body('surname').isLength({ min: 2, max: 40}),
+    body('password').isLength({ min:8, max:32}).isAlphanumeric(),
+    body('dni').isAlphanumeric(),
+    body('phone').isNumeric(),
+    body('address').matches(/[A-Z ]/i), //No funciona el Regex
+    body('email').isEmail().normalizeEmail(),
+    body('age').isNumeric().isLength({ min: 2, max: 3}),
+    body('gender').isAlpha()], userController.signUp); //ok
+router.post('/login', [
+    body('email').isEmail().normalizeEmail(),
+    body('password').isLength({ min:8, max:32}).isAlphanumeric()], userController.login); //
 router.get('/reset-password', userController.restorePassword);
-router.post('/reset-password', userController.resetPasswordPost);
+router.post('/reset-password', [
+    body('password').isLength({ min:8, max:32}).isAlphanumeric()], userController.resetPasswordPost);
 
 // private
 router.get('/', authorise, userController.getUserData);
